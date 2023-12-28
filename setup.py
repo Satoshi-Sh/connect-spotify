4c315e7a6e420ed78764abb69efa6e2b38d98ec5
satoshi-sh <satoss1108@gmail.com>
2023-12-27 21:46:44 -0600

    Added __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="connect_spotify",
-    version="1.0.3",
+    version="1.0.4",
     description="Connect to Spotify API with Streamlit",
     packages=find_packages(),
     include_package_data=True,
